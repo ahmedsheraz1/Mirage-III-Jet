7d1ccceb8f1644d124135f112f46240b56a7d8ec
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-13 23:00:48 +0500

    Create Mirage III Jet.py

---
+++ Mirage III Jet.py
@@ -0,0 +1,6 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'Mirage III Jet').hexdigest()
+'66bb090bf794587a46b5caacc3c2c1d37ea198a5b9cc43c73c6ff4814545329e80ce57eb67a05637cd79c54557acfa19ed9ec31b8eb566fc89104f51efb664a6'
+
